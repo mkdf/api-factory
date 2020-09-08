bb29945dcebbd19f1aadea5796a442132817312e
enridaga <enricodaga@gmail.com>
2020-09-08 18:46:58 +0100

    Changed error reporting to json model

--- module/apif-core/src/Controller/PermissionsManagementController.php
+++ module/apif-core/src/Controller/PermissionsManagementController.php
@@ -42,6 +42,7 @@ class PermissionsManagementController extends AbstractRestfulController
             ];
         }
         else {
+            // FIXME Throw exception and manage response in the action method
             header('WWW-Authenticate: Basic realm="Realm"');
             header('HTTP/1.0 401 Unauthorized');
             echo 'Authentication credentials missing';
@@ -71,8 +72,9 @@ class PermissionsManagementController extends AbstractRestfulController
         $key = $this->params()->fromRoute('id', null);
         if (!$key) {
             $this->getResponse()->setStatusCode(400);
-            echo 'Bad request, missing key in URL';
-            exit();
+            $jm = new JsonModel();
+            $jm->setVariable("message",'Bad request, missing key in URL');
+            return $jm;
         }
         return $this->update($key);
     }
@@ -85,12 +87,13 @@ class PermissionsManagementController extends AbstractRestfulController
         $writeParam = $this->params()->fromQuery('write', null);
         if (is_null($datasetParam) || is_null($readParam) || is_null($writeParam)) {
             $this->getResponse()->setStatusCode(400);
-            echo 'Bad request, missing dataset id or read/write parameters';
-            exit();
+            $jm = new JsonModel();
+            $jm->setVariable("message",'Bad request, missing dataset id or read/write parameters');
+            return $jm;
         }
 
         $this->_repository->setKeyPermissions($id, $datasetParam, $readParam, $writeParam, $auth);
-        return new JsonModel([]);
+        return new JsonModel();
     }
 
     public function delete($id) {
