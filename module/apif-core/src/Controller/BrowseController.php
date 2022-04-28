007b3aba9c3cc2fdb80401f4be4cb6682578a07d
Jason Carvalho <jase@jasoncarvalho.com>
2022-04-28 14:45:23 +0100

    Add access control response headers for preflight OPTIONS requests, to enable JS/AJAX requests from LDH portal

--- module/apif-core/src/Controller/BrowseController.php
+++ module/apif-core/src/Controller/BrowseController.php
@@ -110,6 +110,18 @@ class BrowseController extends AbstractRestfulController
         }
     }
 
+    public function options() {
+        $this->getResponse()->getHeaders()->addHeaders([
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Headers' => '*',
+            'Access-Control-Allow-Methods' => 'GET, OPTIONS',
+            'Access-Control-Max-Age' => '86400',
+
+        ]);
+        return new JsonModel([]);
+    }
+
     /*
  * GET - Handling a GET request
  * brings back all docs from a dataset (subject to limit), or a query
@@ -230,6 +242,9 @@ class BrowseController extends AbstractRestfulController
         $logData = $this->_assembleLogData($datasetUUID, $key, $action, $summary);
         $this->_activityLog->logActivity($logData);
 
+        $this->getResponse()->getHeaders()->addHeaders([
+            'Access-Control-Allow-Origin' => '*'
+        ]);
         return new JsonModel($this->_wrapMetadata($data, $metadata));
     }
 }
